199eba2d286b3cc898fb56956226c6d188a577e6
Stephen <stephen.lyssy@gmail.com>
2022-06-30 20:11:43 -0500

    Got the logic to work as outlined in the assignment.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// uses strict mode so strings are not coerced, variables are not hoisted, etc... 
+// uses strict mode so strings are not coerced, variables are not hoisted, etc...
 'use strict';
 
 // brings in the assert module for unit testing
@@ -8,16 +8,34 @@ const readline = require('readline');
 // use the readline module to print out to the command line
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 // the function that will be called by the unit test below
 const rockPaperScissors = (hand1, hand2) => {
+  // should detect a tie
+  // should detect which hand won
+  // should scrub input to ensure lowercase with "trim"ed whitespace
 
-  // Write code here
-  // Use the unit test to see what is expected
+  let userOne = hand1.toLowerCase().trim();
+  let userTwo = hand2.toLowerCase().trim();
 
-}
+  if (userOne === userTwo) {
+    return "It's a tie!";
+  } else if (
+    (userOne === 'rock' && userTwo === 'scissors') ||
+    (userOne === 'paper' && userTwo === 'rock') ||
+    (userOne === 'scissor' && userTwo === 'paper')
+  ) {
+    return 'Hand one wins!';
+  } else if (
+    (userOne === 'rock' && userTwo === 'paper') ||
+    (userOne === 'paper' && userTwo === 'scissors') ||
+    (userOne === 'scissor' && userTwo === 'rock')
+  ) {
+    return 'Hand two wins!';
+  }
+};
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
@@ -25,17 +43,17 @@ const rockPaperScissors = (hand1, hand2) => {
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
     rl.question('hand2: ', (answer2) => {
-      console.log( rockPaperScissors(answer1, answer2) );
+      console.log(rockPaperScissors(answer1, answer2));
       getPrompt();
     });
   });
 }
 
+//# Start Mocha Test Section
 // Unit Tests
 // to use them run the command: npm test main.js
 // to close them ctrl + C
 if (typeof describe === 'function') {
-
   // most are notes for human eyes to read, but essentially passes in inputs then compares if the function you built returns the expected output.
   describe('#rockPaperScissors()', () => {
     it('should detect a tie', () => {
@@ -44,19 +62,17 @@ if (typeof describe === 'function') {
       assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
     });
     it('should detect which hand won', () => {
-      assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
-      assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
+      assert.equal(rockPaperScissors('rock', 'paper'), 'Hand two wins!');
+      assert.equal(rockPaperScissors('paper', 'scissors'), 'Hand two wins!');
+      assert.equal(rockPaperScissors('rock', 'scissors'), 'Hand one wins!');
     });
     it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
-      assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
-      assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
+      assert.equal(rockPaperScissors('rOcK', ' paper '), 'Hand two wins!');
+      assert.equal(rockPaperScissors('Paper', 'SCISSORS'), 'Hand two wins!');
+      assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), 'Hand one wins!');
     });
   });
 } else {
-
   // always returns ask the user for another input
   getPrompt();
-
 }
